ba078bc9f16b19b23046819bff7e4aba399f3180
Melissa Loos <melissa.loos@uni-ulm.de>
2021-01-15 20:09:49 +0100

    create bookmarklet

--- alphabeticSort.js
+++ alphabeticSort.js
@@ -1,19 +1,21 @@
-var container = $('#filmstripRemoteVideosContainer')[0];
-var jChildren = $(container).children();
-const numberOfVideos = jChildren.length;
-var names = new Array();
+function(){
+  var container = $('#filmstripRemoteVideosContainer')[0];
+  var jChildren = $(container).children();
+  const numberOfVideos = jChildren.length;
+  var names = new Array();
 
-//only applicable in tiles mode!
-for(i=0; i<numberOfVideos; i++) {
-  names[i] = new Array (2);
-  names[i][0] = jChildren[i].getElementsByClassName("displayname")[0].innerHTML;
-  names[i][1] = jChildren[i];
-}
+  //only applicable in tiles mode!
+  for(i=0; i<numberOfVideos; i++) {
+    names[i] = new Array (2);
+    names[i][0] = jChildren[i].getElementsByClassName("displayname")[0].innerHTML;
+    names[i][1] = jChildren[i];
+  }
 
-//sort Array
-names.sort((a, b) => a[0].localeCompare(b[0]))
+  //sort Array
+  names.sort((a, b) => a[0].localeCompare(b[0]));
 
-//reorder the tiles
-for(i = 0; i < numberOfVideos; i++){
-  $(names[i][1]).css('order', i);
+  //reorder the tiles
+  for(i = 0; i < numberOfVideos; i++){
+    $(names[i][1]).css('order', i);
+  }
 }
